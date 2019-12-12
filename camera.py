1e0c6ae9f5a2de5b36c755568a9d27bf21cf2291
S-shinzi <b8p31049@shonan.bunkyo.ac.jp>
2019-12-13 01:08:46 +0900

    画像の切り抜きと位置合わせ用の補助線を表示するように変更

--- camera.py
+++ camera.py
@@ -15,9 +15,20 @@ def save_frame_camera_key(device_num, dir_path, basename, ext='jpg', delay=1, wi
 
         cap_cam = cv2.VideoCapture(1)
         ret, frame = cap.read()
-        cv2.imshow(window_name, frame)
+        # 表示用(補助線あり)とキャプチャ用の画像を分ける
+        edframe = frame
+        # 補助線を引く
+        cv2.rectangle(edframe, (120,116), (524,360),(0,0,255),3)
+        # 表示
+        cv2.imshow(window_name, edframe)
         key = cv2.waitKey(delay) & 0xFF
         if key == ord('c'):
+            # 画像切り抜き img[top : bottom, left : right]
+            frame = frame[116 : 360, 120 : 524]
+
+            # 切り抜いた画像を上下反転
+            frame = cv2.rotate(frame, cv2.ROTATE_180)
+
             cv2.imwrite('{}_{}.{}'.format(base_path, n, ext), frame)
             n += 1
         elif key == ord('q'):
